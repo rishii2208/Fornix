90cf8acfa21d6b7e169e03787165a751e31cd7e9
rishii2208 <rishirajprajapati22@gmail.com>
2025-06-17 18:08:15 +0530

    first commit

---
+++ src/components/design/MyName.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="sr-only">Fornix</h1>
+  <h2 class="sr-only">Develpoment Agency</h2>
+
+  <MyEnName id="svg-my-en-name" :class="$attrs.class" />
+</template>
+
+<script setup lang="ts">
+  import { MyEnName } from '..';
+</script>
